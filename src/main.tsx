e6861dab84d2e820b2fb3e39274b9f2f09eb3880
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2023-08-06 12:43:14 +0000

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:gamecode",
+    element: <GameSession />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/pages/GameSession.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+interface IGameSessionProps {}
+
+export const GameSession: FC<IGameSessionProps> = (props) => {
+  return <div></div>;
+};
